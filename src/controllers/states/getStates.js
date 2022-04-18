9b5b652453e2377e308ee4666cffed9f6a90b0d9
Giovani Eduardo <giovaniequisini@gmail.com>
2022-04-18 15:43:37 -0300

    hotfix: Add querytype to get return

--- src/controllers/states/getStates.js
+++ src/controllers/states/getStates.js
@@ -1,8 +1,11 @@
+const { QueryTypes } = require('sequelize')
 const sequelize = require('./../../database/database')
 
 module.exports = {
   async getStates (_, res) {
-    const results = await sequelize.query('SELECT * FROM states')
+    const results = await sequelize.query('SELECT * FROM states', {
+      type: QueryTypes.SELECT
+    })
 
     return res.json(results)
   }
